d087ed82e65b0d39740f7a97406cdb70255612f6
Dayu0501 <15618623090@163.com>
2020-09-03 22:03:04 +0800

    daily update

--- STL_alg/algorithmUnity.h
+++ STL_alg/algorithmUnity.h
@@ -9,6 +9,7 @@
 
 using namespace std;
 
+
 //function declare
 template <class ForWardIter, class T>
 ForWardIter DY_remove_copy(ForWardIter first, ForWardIter last, ForWardIter result, const T &val);
@@ -16,29 +17,52 @@ ForWardIter DY_remove_copy(ForWardIter first, ForWardIter last, ForWardIter resu
 template <class ForWardIter, class T>
 ForWardIter DY_remove(ForWardIter first, ForWardIter last, const T &val);
 
-//template <class ForWardIter, class T>
-//ForWardIter DY_remove_if(ForWardIter first, ForWardIter last, const T &val);
+template <class ForWardIter, class Predicate>
+ForWardIter DY_remove_copy_if(ForWardIter first, ForWardIter last, ForWardIter result, Predicate pred);
+
+template <class ForWardIter, class Predicate>
+ForWardIter DY_remove_if(ForWardIter first, ForWardIter last, Predicate pred);
+
 
 //function define
 template <class ForWardIter, class T>
 ForWardIter DY_remove(ForWardIter first, ForWardIter last, const T &val) {
-	first = find(first, last, val);
-	ForWardIter next = first;
+    first = find(first, last, val);
+    ForWardIter next = first;
 
-	return first == last ? first : DY_remove_copy(++next, last, first, val);
+    return first == last ? first : DY_remove_copy(++next, last, first, val);
 
-	return first;
+    return first;
 }
 
 template <class ForWardIter, class T>
 ForWardIter DY_remove_copy(ForWardIter first, ForWardIter last, ForWardIter result, const T &val) {
-	while (first != last) {
-		if (*first != val) *result++ = *first;
-		first++;
-	}
+    while (first != last) {
+        if (*first != val) *result++ = *first;
+        first++;
+    }
 
-	return result;
+    return result;
 }
 
+template <class ForWardIter, class Predicate>
+ForWardIter DY_remove_copy_if(ForWardIter first, ForWardIter last, ForWardIter result, Predicate pred) {
+    while (first != last) {
+        if (!pred(*first)) {
+            *result++ = *first;
+        }
+        first++;
+    }
+
+    return first;
+}
+
+template <class ForWardIter, class Predicate>
+ForWardIter DY_remove_if(ForWardIter first, ForWardIter last, Predicate pred){
+    first = find_if(first, last, pred);
+    ForWardIter next = first;
+
+    return first == last ? first : DY_remove_copy_if(++next, last, first, pred);
+}
 
 #endif //STL_ALG_ALGORITHMUNITY_H
--- STL_alg/main.cpp
+++ STL_alg/main.cpp
@@ -3,16 +3,29 @@
 
 void test_DY_remove () {
 	cout << "test remove start !" << endl;
-	string testDate_0 = "465875906556";
+	string testData_0 = "465875906556";
 	char b = '5';
 
-	cout << "operation data is " << testDate_0 << endl;
+	cout << "operation data is " << testData_0 << endl;
 	cout << "Need to delete is " << b << endl;
 
-	testDate_0.erase(DY_remove(testDate_0.begin(), testDate_0.end(), b), testDate_0.end());
+    testData_0.erase(DY_remove(testData_0.begin(), testData_0.end(), b), testData_0.end());
 
-	cout << "result = " << testDate_0 << endl;
+	cout << "result = " << testData_0 << endl;
 }
+
+void test_DY_remove_if() {
+    cout << "test remove_copy_if test" << endl;
+
+    string testData_0 {"465 87 5906 556"};
+    cout << "operation data is " << testData_0 << endl;
+
+    testData_0.erase(DY_remove_if(testData_0.begin(), testData_0.end(), [](unsigned char x){ return std::isspace(x); }), testData_0.end());
+
+    cout << "result = " << testData_0 << endl;
+}
+
+#define DY_REMOVE_IF
 
 int main() {
 
@@ -20,7 +33,9 @@ int main() {
 	test_DY_remove();
 #endif
 
-	vector<string, my_alloc::allocator<string>> ins {"hello", "world"};
+#ifdef DY_REMOVE_IF
+    test_DY_remove_if();
+#endif
 
 
 
