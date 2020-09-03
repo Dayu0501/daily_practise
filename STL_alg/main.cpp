fb49f2ad80ba9783d569153aa581cd8f4a7350b1
sunchao <sunchao@em-data.com.cn>
2020-09-03 20:26:14 +0800

    daily update

--- STL_alg/main.cpp
+++ STL_alg/main.cpp
@@ -1,5 +1,8 @@
 #include "algorithmUnity.h"
 #include "myalloc.h"
+#include <time.h>
+#include <ctime>
+#include <stdio.h>
 
 void test_DY_remove () {
 	cout << "test remove start !" << endl;
@@ -14,13 +17,40 @@ void test_DY_remove () {
 	cout << "result = " << testDate_0 << endl;
 }
 
+std::string getNowDate()
+{
+	time_t t;
+	char buf[64];
+
+	/* 获取时间 */
+	time(&t);
+	strftime(buf, sizeof(buf), "%F", localtime(&t));
+	return buf;
+}
+
+std::string getDangTianRiQi()
+{
+	std::string nowTime;
+	std::time_t t = std::time(NULL);
+	std::tm *st = std::localtime(&t);
+	char tmpArray[64] = { 0 };
+	sprintf(tmpArray, "%d-%02d-%02d", st->tm_year + 1900, st->tm_mon + 1, st->tm_mday);
+
+	nowTime = tmpArray;
+
+	return nowTime;
+}
+
 int main() {
 
 #ifdef DY_REMOVE
 	test_DY_remove();
 #endif
 
-	vector<string, my_alloc::allocator<string>> ins {"hello", "world"};
+	//vector<string, my_alloc::allocator<string>> ins {"hello", "world"};
+
+	cout << "date is " << getNowDate() << endl;
+	cout << "getDangTianRiQi is " << getDangTianRiQi() << endl;
 
 
 
