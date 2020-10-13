11a92ecf2fdfc75e3ad543ff67cb03fe12a12b28
Dayu0501 <15618623090@163.com>
2020-10-13 23:15:51 +0800

    daily update

--- programming_learn/virtual_inherit.cpp
+++ programming_learn/virtual_inherit.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
-#include <queue>
 
 using namespace std;
 
+class A {
+public:
+    int a;
+};
+
+class B : virtual public A {
+public :
+    int bb;
+};
+
+class C : virtual public A {
+public:
+    int cc;
+};
+
+class D : public B, public C {
+public :
+    void print() {
+        cout << "A::a = " << a << endl;
+    }
+};
+
+
 int main() {
-	cout << "i am virtual inherit !" << endl;
-	return 0;
+    cout << "----- [ 虚继承测试 ] ------" << endl;
+    D ins;
+    ins.print();
+
+    return 0;
 }
