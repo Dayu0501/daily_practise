9f8da7f5725593caa6f9e16575609581bacf128d
Dayu0501 <15618623090@163.com>
2020-10-14 23:50:02 +0800

    daily

--- programming_learn/testtest.cpp
+++ programming_learn/testtest.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <memory>
 
 using namespace std;
 
@@ -11,14 +14,19 @@ public :
 	~AA() {
 		cout << "I am destruct !" << endl;
 	}
+
+	string aa;
+    string bb;
 };
 
 int main() {
 	cout << "----- [ 测试析构函数是否会被调用 ] -----" << endl;
 	AA * ptr = new AA;
 
+	//vector<AA, __gun_cxx::__pool_allo>
+	vector<string, __gun_cxx::__pool_alloc<string>> testStr;
+
 
 
 	return 0;
 }
-
