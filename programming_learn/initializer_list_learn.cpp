5c14f1661203dc35492448a986ae452eb7a3cfe0
sunchao <sunchao@em-data.com.cn>
2020-10-30 18:00:42 +0800

    daily update

--- programming_learn/initializer_list_learn.cpp
+++ programming_learn/initializer_list_learn.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <initializer_list>
+
 using namespace std;
 
 class myClass {
@@ -8,7 +9,7 @@ public:
 		cout << "I am initializer_list !" << endl;
 	}
 
-    myClass(int a, int b, int c) {
+	myClass(int a, int b, int c) {
 		cout << "param list con !" << endl;
 	}
 
@@ -18,14 +19,39 @@ public:
 };
 
 
+template <typename T>
+class smart_ptr {
+public:
+	explicit smart_ptr(T* ptr = nullptr)
+		: ptr_(ptr) {}
+	~smart_ptr()
+	{
+		delete ptr_;
+	}
+	T* get() const { return ptr_; }
+private:
+	T* ptr_;
+};
+
 int main() {
 	/*
 	 * 所以验证了侯捷的一个例子是错误的，维持原判*/
-	myClass kaka {1, 2, 3};
+	myClass kaka{1, 2, 3};
 	myClass haha = {1, 2, 3};
 	myClass bubu({1, 2, 3});
 
 	myClass ins = kaka;
+
+	string hello = "hello";
+	int dd = 1;
+	switch (dd) {
+		case 2:
+			cout << "haha" << endl;
+			break;
+		case 3:
+			cout << "bubu" << endl;
+			break;
+	}
 
 	return 0;
 }
