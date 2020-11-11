34aa292a39ac79e68b1bef9b1e52d874fbc79e87
sunchao <sunchao@em-data.com.cn>
2020-11-11 20:33:34 +0800

    daily update

---
+++ design_pattern/cmake-build-debug/CMakeFiles/design_mode.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/design_mode.dir/Component_collaboration/observer/MainForm1.cpp.obj"
+  "CMakeFiles/design_mode.dir/Component_collaboration/observer/MainForm2.cpp.obj"
+  "CMakeFiles/design_mode.dir/Component_collaboration/observer/test/new_observer.cpp.obj"
+  "CMakeFiles/design_mode.dir/Component_collaboration/observer/test/observer.cpp.obj"
+  "CMakeFiles/design_mode.dir/Component_collaboration/strategy/strategy1.cpp.obj"
+  "CMakeFiles/design_mode.dir/Component_collaboration/strategy/strategy2.cpp.obj"
+  "CMakeFiles/design_mode.dir/Component_collaboration/template_method/template1_app.cpp.obj"
+  "CMakeFiles/design_mode.dir/Component_collaboration/template_method/template2_app.cpp.obj"
+  "CMakeFiles/design_mode.dir/Single_responsibility/brigde/bridge1.cpp.obj"
+  "CMakeFiles/design_mode.dir/Single_responsibility/brigde/bridge2.cpp.obj"
+  "CMakeFiles/design_mode.dir/Single_responsibility/decorator/decorator1.cpp.obj"
+  "CMakeFiles/design_mode.dir/Single_responsibility/decorator/decorator2.cpp.obj"
+  "CMakeFiles/design_mode.dir/Single_responsibility/decorator/decorator3.cpp.obj"
+  "design_mode.exe"
+  "design_mode.exe.manifest"
+  "design_mode.pdb"
+  "libdesign_mode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/design_mode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ design_pattern/cmake-build-debug/CMakeFiles/new_observer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/new_observer.dir/Component_collaboration/observer/test/new_observer.cpp.obj"
+  "libnew_observer.dll.a"
+  "new_observer.exe"
+  "new_observer.exe.manifest"
+  "new_observer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/new_observer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
